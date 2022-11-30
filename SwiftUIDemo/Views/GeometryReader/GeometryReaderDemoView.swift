0af239b20dafc3c17267a5021b2e00de1e8d65d1
e.b.olson <e.b.olson.mobile@gmail.com>
2022-11-29 17:59:15 -0800

    Added GeometryReader demo view

--- SwiftUIDemo/Views/GeometryReader/GeometryReaderDemoView.swift
+++ SwiftUIDemo/Views/GeometryReader/GeometryReaderDemoView.swift
@@ -10,8 +10,17 @@ import SwiftUI
 struct GeometryReaderDemoView: View {
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(GeometryReaderDemoView)")
+        /*
+         The GeometryReader is one of my favorite "views"
+         
+         A GeometryReader will take up as much space as it is allowed.
+         (i.e. it "grows" to the largest possible size)
+         */
+        
+        GeometryReader { geometry in
+            Text("GeometryReader is a container view that defines its content as a function of its own size and coordinate space.")
+                .frame(width: geometry.size.width / 2.0)
+        }
     }
 }
 
