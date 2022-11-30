6f67eb13e453ad7a71b6e88b8629cb73aff73e3a
e.b.olson <e.b.olson.mobile@gmail.com>
2022-11-30 14:49:47 -0800

    Adding Group demo view.

--- SwiftUIDemo/Views/Group/GroupDemoView.swift
+++ SwiftUIDemo/Views/Group/GroupDemoView.swift
@@ -10,13 +10,34 @@ import SwiftUI
 struct GroupDemoView: View {
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(GroupDemoView)")
+        VStack(alignment: .leading) {
+            Group {
+                Text("Groups allow for grouping of views.")
+                Text("Modifiers applied to the Group get applied to all subviews in the Group!")
+            }
+            .multilineTextAlignment(.leading)
+            .foregroundColor([.cyan, .mint, .indigo, .teal, .red, .purple].randomElement()!)
+            .padding(8)
+            .font(.title2)
+        }
     }
 }
 
 struct GroupDemoView_Previews: PreviewProvider {
     static var previews: some View {
-        GroupDemoView()
+        /*
+         My favorite use for the Group view is in the PreviewProvider.
+         
+         Xcode will create a new tab/window in the canvas (preview area)
+         for each view in the Group.  Super useful!
+         */
+        Group {
+            GroupDemoView()
+                .previewDisplayName("Group Demo 1")
+            GroupDemoView()
+                .previewDisplayName("Group Demo 2")
+            GroupDemoView()
+                .previewDisplayName("Group Demo 3")
+        }
     }
 }
