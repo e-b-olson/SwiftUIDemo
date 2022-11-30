8449ad80056e7d13d26a2150fbd1d1dd8c7798d3
e.b.olson <e.b.olson.mobile@gmail.com>
2022-11-30 12:03:39 -0800

    Added EditButton demo view

--- SwiftUIDemo/Views/EditButton/EditButtonDemoView.swift
+++ SwiftUIDemo/Views/EditButton/EditButtonDemoView.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 struct EditButtonDemoView: View {
     
+    @State private var bountyHunters = [
+        "4-LOM",
+        "Boba Fett",
+        "Bossk",
+        "Dengar",
+        "IG-88",
+        "Zuckuss"
+    ]
+    
     var body: some View {
-        Text("Placeholder View")
-        Text("(EditButtonDemoView)")
+        List {
+            ForEach(bountyHunters, id: \.self) { bountyHunter in
+                Text(bountyHunter)
+            }
+            /*
+             Try adding/removing the `onDelete` and `onMove` modifiers to
+             see what happens.
+             */
+            .onDelete { bountyHunters.remove(atOffsets: $0) }
+            .onMove { bountyHunters.move(fromOffsets: $0, toOffset: $1) }
+        }
+        .toolbar {
+            /*
+             The EditButton toggles the `editMode` of the environment.
+
+             Different controls will be displayed in `editMode` depending
+             on whether or not the `onMove` or `onDelete` modifiers are
+             applied to the editable view.
+             */
+            EditButton()
+        }
+
     }
 }
 
