2d2fb05c7724b5398360c00a6cdc78a5d046eb38
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-01 15:26:32 -0800

    Adding LazyVStack demo view.

--- SwiftUIDemo/Views/LazyVStack/LazyVStackDemoView.swift
+++ SwiftUIDemo/Views/LazyVStack/LazyVStackDemoView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct LazyVStackDemoView: View {
+    /*
+     Text adapted from Apple's documentation
+     https://developer.apple.com/documentation/swiftui/lazyvstack
+     */
+    private let text = "The stack is 'lazy,' in that the stack view doesn’t create items until it needs to render them onscreen."
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(LazyVStackDemoView)")
+        VStack {
+            Text("A view that arranges its children in a line that grows vertically, creating items only as needed.")
+                .padding(12)
+            
+            ScrollView { //<-- wrap your LazyVStack in a vertical ScrollView
+                LazyVStack {
+                    ForEach(Array(text.split(separator: " ").enumerated()), id: \.offset) { offset, item in
+                        Text(item)
+                            .padding(5)
+                            .border([.cyan, .blue, .indigo, .purple, .teal].randomElement()!)
+                    }
+                }
+            }
+            .padding(.horizontal, 12)
+        }
     }
 }
 
