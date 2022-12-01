4abfd301bc6f0c493f5ac7b2377a0f98b4472144
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-01 15:22:47 -0800

    Adding LazyVGrid demo view.

--- SwiftUIDemo/Views/LazyVGrid/LazyVGridDemoView.swift
+++ SwiftUIDemo/Views/LazyVGrid/LazyVGridDemoView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct LazyVGridDemoView: View {
+    // Define your grid columns
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
     
+    /*
+     This code is adapted from Apple's documentation
+     https://developer.apple.com/documentation/swiftui/lazyvgrid
+     */
     var body: some View {
-        Text("Placeholder View")
-        Text("(LazyVGridDemoView)")
+        ScrollView { //<-- wrap your LazyVGrid in a vertical ScrollView
+            LazyVGrid(columns: columns) {
+                ForEach(0x1f600...0x1f650, id: \.self) { value in
+                    Text(String(format: "%x", value))   //<-- column 0
+                    Text(emoji(value))                  //<-- column 1
+                        .font(.largeTitle)
+                }
+            }
+        }
+    }
+    
+    private func emoji(_ value: Int) -> String {
+        guard let scalar = UnicodeScalar(value) else { return "?" }
+        return String(Character(scalar))
     }
 }
 
