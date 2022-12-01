e1073658fcdc135665e71d85b67ab30a796fa682
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-01 14:38:23 -0800

    Adding LazyHGrid demo view

--- SwiftUIDemo/Views/LazyHGrid/LazyHGridDemoView.swift
+++ SwiftUIDemo/Views/LazyHGrid/LazyHGridDemoView.swift
@@ -8,10 +8,29 @@
 import SwiftUI
 
 struct LazyHGridDemoView: View {
+    // Define your grid rows
+    let rows = [GridItem(.fixed(30)), GridItem(.fixed(30))]
     
+    /*
+     This code is adapted from Apple's documentation
+     https://developer.apple.com/documentation/swiftui/lazyhgrid
+     */
     var body: some View {
-        Text("Placeholder View")
-        Text("(LazyHGridDemoView)")
+        ScrollView(.horizontal) { //<-- wrap your LazyHGrid in a ScrollView(.horizontal)
+            LazyHGrid(rows: rows) {
+                ForEach(0x1f600...0x1f650, id: \.self) { value in
+                    Text(String(format: "%x", value))   //<-- row 0
+                    Text(emoji(value))                  //<-- row 1
+                        .font(.largeTitle)
+                }
+            }
+        }
+        
+    }
+    
+    private func emoji(_ value: Int) -> String {
+        guard let scalar = UnicodeScalar(value) else { return "?" }
+        return String(Character(scalar))
     }
 }
 
