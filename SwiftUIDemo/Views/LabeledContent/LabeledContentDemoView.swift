2eab3fd56a9159119074dd0cb469c1ba4a99f495
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-01 13:15:10 -0800

    Adding LabeledContent demo view (note: iOS 16+ only).

--- SwiftUIDemo/Views/LabeledContent/LabeledContentDemoView.swift
+++ SwiftUIDemo/Views/LabeledContent/LabeledContentDemoView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct LabeledContentDemoView: View {
+    @State private var selection: Int = 0
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(LabeledContentDemoView)")
+        /*
+         This code is adapted from Apple's documentation
+         https://developer.apple.com/documentation/swiftui/labeledcontent
+         
+         Note: LabeledContent is only available in iOS 16.0+
+         */
+        if #available(iOS 16, *) {
+            Form {
+                LabeledContent("Labeled Content") {
+                    CustomSelectionValueView(value: $selection)
+                }
+                Picker("Selected Value", selection: $selection) {
+                    Text("Option 1").tag(1)
+                    Text("Option 2").tag(2)
+                }
+            }
+        } else {
+            Text("Placeholder View")
+            Text("(LabeledContent only available in iOS 16+)")
+        }
+    }
+}
+
+struct CustomSelectionValueView: View {
+    @Binding var value: Int
+    var body: some View {
+        Text("\(value)")
     }
 }
 
