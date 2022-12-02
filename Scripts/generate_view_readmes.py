8d98b548fda3ed85a20dbc9778cfa0f00ace8d94
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-02 15:53:29 -0800

    Adding NavigationView demo view

---
+++ Scripts/generate_view_readmes.py
@@ -0,0 +1,51 @@
+import os
+
+def main():
+    # get list of views
+    list_of_views = get_view_list()
+
+    # for each view
+    for view_name in list_of_views:
+        if not view_name:
+            continue
+        
+        create_view_readme_file(view_name)
+
+
+def get_view_list():
+    view_list_file_path = "./view_list.txt"
+
+    with open(view_list_file_path, 'r') as view_list_file:
+        view_list_data = view_list_file.read()
+
+    view_list = view_list_data.split('\n')
+        
+    view_list_file.close()
+
+    return view_list
+
+
+def create_view_readme_file(view_name):
+    # args
+    template_view_name = "{{ view_name }}"
+    template_file_path = "./templates/view_readme_template.swift"
+    output_file_path = "./SwiftUIDemo/Views/" + view_name + "/" + "README.md"
+
+    if os.path.exists(output_file_path):
+        print("File already exists. Skipping: {}".format(output_file_path))
+    
+    with open(template_file_path, 'r') as template_file:
+        template_data = template_file.read()
+
+    # replace template strings
+    file_data = template_data.replace(template_view_name, view_name)
+
+    # write file
+    with open(output_file_path, 'w') as output_file:
+        output_file.write(file_data)
+
+    output_file.close()
+    template_file.close()
+
+if __name__ == '__main__':
+    main()
--- SwiftUIDemo/Views/NavigationView/NavigationViewDemoView.swift
+++ SwiftUIDemo/Views/NavigationView/NavigationViewDemoView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct NavigationViewDemoView: View {
+    private let colors: [Color] = [.purple, .indigo, .blue, .cyan, .teal]
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(NavigationViewDemoView)")
+        /*
+         Note: NavigationView is now deprecated (as of iOS 16.1)
+         Use NavigationStack or NavigationSplitView instead.
+         */
+        if #available(iOS 16, *) {
+            GroupBox {
+                Group {
+                    Text("NavigationView is deprecated as of iOS 16.1")
+                        .padding(.top, 1)
+                        .padding(.bottom, 5)
+                    Text("Use NavigationStack or NavigationSplitView instead, as best fits your needs.")
+                }
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal, 2)
+            } label: {
+                Label("NavigationView Deprecated", systemImage: "info.circle")
+                    .foregroundColor(Color.blue)
+            }
+        } else {
+            NavigationView {
+                List(colors, id: \.self) { color in
+                    NavigationLink {
+                        Text(color.description)
+                            .foregroundColor(color)
+                    } label: {
+                        Text(color.description)
+                    }
+                }
+            }
+        }
     }
 }
 
