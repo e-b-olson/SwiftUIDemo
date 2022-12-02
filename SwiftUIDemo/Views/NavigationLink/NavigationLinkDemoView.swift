03e217986cd68b641b45213137bf3624ff2b3995
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-02 12:16:23 -0800

    Adding NavigationLink demo view

--- SwiftUIDemo/Views/NavigationLink/NavigationLinkDemoView.swift
+++ SwiftUIDemo/Views/NavigationLink/NavigationLinkDemoView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct NavigationLinkDemoView: View {
+    @State private var type: ContentViewModel.ViewType = ContentViewModel.ViewType.allCases.randomElement()!
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(NavigationLinkDemoView)")
+        Button("Randomize NavigationLink", action: { type =  ContentViewModel.ViewType.allCases.randomElement()!})
+            .buttonStyle(.borderedProminent)
+            .padding(.bottom, 32)
+
+        NavigationLink {
+            DemoView(type: type)
+        } label: {
+            Text(type.rawValue)
+        }
     }
 }
 
 struct NavigationLinkDemoView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationLinkDemoView()
+        /*
+         NavigationLinks need to be inside a NavigationView.
+         
+         In this app, the top level view (ContentView) wraps
+         the subviews in a NavigationView.
+         */
+        NavigationView {
+            VStack {
+                NavigationLinkDemoView()
+            }
+        }
     }
 }
