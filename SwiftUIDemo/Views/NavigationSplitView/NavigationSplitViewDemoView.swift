fce45fe6eeecf58e1fd518bb03d8f759247906d4
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-02 15:07:59 -0800

    Adding NavigationSplitView demo view

--- SwiftUIDemo/Views/NavigationSplitView/NavigationSplitViewDemoView.swift
+++ SwiftUIDemo/Views/NavigationSplitView/NavigationSplitViewDemoView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct NavigationSplitViewDemoView: View {
+    @State private var selectedColor: Color?
+    private let colors: [Color] = [.purple, .indigo, .blue, .cyan, .teal]
+    
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(NavigationSplitViewDemoView)")
+        /*
+         NavigationSplitView is currently only available in iOS 16.0+
+         */
+        if #available(iOS 16, *) {
+            NavigationSplitView {
+                List(colors, id: \.self, selection: $selectedColor) { color in
+                    Text(color.description)
+                }
+            } detail: {
+                Text(selectedColor?.description ?? "No Selection")
+                    .foregroundColor(selectedColor ?? .black)
+            }
+        } else {
+            Text("Placeholder View")
+            Text("(NavigationSplitView only available in iOS 16+)")
+        }
     }
 }
 
