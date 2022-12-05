ab192522e32356fe780b82d24341109a45cf6d28
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-05 14:18:20 -0800

    Adding PasteButton demo view

--- SwiftUIDemo/Views/PasteButton/PasteButtonDemoView.swift
+++ SwiftUIDemo/Views/PasteButton/PasteButtonDemoView.swift
@@ -8,10 +8,28 @@
 import SwiftUI
 
 struct PasteButtonDemoView: View {
+    @State private var pastedText: String = ""
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(PasteButtonDemoView)")
+        /*
+         This code is adapted from Apple's documentation
+         https://developer.apple.com/documentation/swiftui/pastebutton
+         
+         Note: PasteButton is currently only available in iOS 16.0+
+         */
+        if #available(iOS 16, *) {
+            HStack {
+                PasteButton(payloadType: String.self) { strings in
+                    pastedText = strings[0]
+                }
+                Divider()
+                Text(pastedText)
+                Spacer()
+            }
+        } else {
+            Text("Placeholder View")
+            Text("(PasteButtonDemoView only available in iOS 16+)")
+        }
     }
 }
 
