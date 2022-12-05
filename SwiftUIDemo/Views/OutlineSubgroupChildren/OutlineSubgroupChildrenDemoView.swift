af4b2f82f2a62873c3e77e7b000f014c978122dd
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-05 11:45:26 -0800

    Adding OutlineSubgroupChildren demo view (kinda)

--- SwiftUIDemo/Views/OutlineSubgroupChildren/OutlineSubgroupChildrenDemoView.swift
+++ SwiftUIDemo/Views/OutlineSubgroupChildren/OutlineSubgroupChildrenDemoView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 struct OutlineSubgroupChildrenDemoView: View {
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(OutlineSubgroupChildrenDemoView)")
+        /*
+         This text is adapted from Apple's documentation
+         https://developer.apple.com/documentation/swiftui/outlinesubgroupchildren
+         */
+        VStack(alignment: .leading) {
+            Text("A type-erased view representing the children in an outline subgroup.")
+                .padding(16)
+            Text("(OutlineGroup uses this type as a generic constraint for the Content of the DisclosureGroup instances it creates.)")
+                .padding(16)
+        }
     }
 }
 
