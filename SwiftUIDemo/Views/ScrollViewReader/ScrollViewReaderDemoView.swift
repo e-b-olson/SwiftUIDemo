0455ddf71ef95d1004fa478d264f846808ec5a1c
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-06 13:55:55 -0800

    Adding ScrollViewReader demo view

--- SwiftUIDemo/Views/ScrollViewReader/ScrollViewReaderDemoView.swift
+++ SwiftUIDemo/Views/ScrollViewReader/ScrollViewReaderDemoView.swift
@@ -8,10 +8,43 @@
 import SwiftUI
 
 struct ScrollViewReaderDemoView: View {
+    @Namespace var topID
+    @Namespace var bottomID
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(ScrollViewReaderDemoView)")
+        /*
+         This code is adapted from Apple's documentation
+         https://developer.apple.com/documentation/swiftui/scrollviewreader
+         */
+        
+        ScrollViewReader { proxy in
+            ScrollView {
+                Button("Scroll to Bottom") {
+                    withAnimation {
+                        proxy.scrollTo(bottomID)
+                    }
+                }
+                .id(topID)
+                
+                VStack(spacing: 0) {
+                    ForEach(0..<100) { i in
+                        color(fraction: Double(i) / 100)
+                            .frame(height: 32)
+                    }
+                }
+                
+                Button("Return to Top") {
+                    withAnimation {
+                        proxy.scrollTo(topID)
+                    }
+                }
+                .id(bottomID)
+            }
+        }
+    }
+    
+    func color(fraction: Double) -> Color {
+        Color(red: 0.5, green: 1 - fraction, blue: fraction)
     }
 }
 
