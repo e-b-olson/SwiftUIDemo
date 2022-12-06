5ab2557d0d9142724f956e14dc416d1367269254
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-06 14:20:09 -0800

    Adding SecureField demo view

--- SwiftUIDemo/Views/SecureField/SecureFieldDemoView.swift
+++ SwiftUIDemo/Views/SecureField/SecureFieldDemoView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct SecureFieldDemoView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(SecureFieldDemoView)")
+        /*
+         This code is adapted from Apple's documentation
+         https://developer.apple.com/documentation/swiftui/securefield
+         */
+
+        VStack {
+            TextField(
+                "User name (email address)",
+                text: $username)
+            .autocapitalization(.none)
+            .disableAutocorrection(true)
+            .border(Color(UIColor.separator))
+
+            SecureField("Password", text: $password) {
+                // SecureField `onCommit` closure
+                handleLogin(username: username, password: password)
+            }
+            .border(Color(UIColor.separator))
+        }
+        .padding(.horizontal, 32)
+    }
+    
+    func handleLogin(username: String, password: String) {
+        debugPrint("Credentials")
+        debugPrint("username: \(username)")
+        debugPrint("password: \(password)")
     }
 }
 
