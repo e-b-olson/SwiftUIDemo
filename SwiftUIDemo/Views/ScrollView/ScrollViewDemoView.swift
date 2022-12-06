8fe23a24acd15141e0b2b4512877830def3882de
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-06 12:00:25 -0800

    Adding ScrollView demo view

--- SwiftUIDemo/Views/ScrollView/ScrollViewDemoView.swift
+++ SwiftUIDemo/Views/ScrollView/ScrollViewDemoView.swift
@@ -10,8 +10,38 @@ import SwiftUI
 struct ScrollViewDemoView: View {
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(ScrollViewDemoView)")
+        VStack(alignment: .leading) {
+            Spacer()
+            Text("Horizontal")
+            ScrollView(.horizontal) {                       //<-- scroll left/right
+                LazyHStack {                                //<-- use HStack
+                    ForEach(0x1f600...0x1f650, id: \.self) { value in
+                        Text(emoji(value))
+                            .font(.largeTitle)
+                    }
+                }
+                .fixedSize()
+            }
+            
+            Spacer()
+            
+            Text("Vertical")
+            ScrollView {                                    //<-- scroll up/down
+                LazyVStack {                                //<-- use VStack
+                    ForEach(0x1f600...0x1f650, id: \.self) { value in
+                        Text(emoji(value))
+                            .font(.largeTitle)
+                    }
+                }
+                .fixedSize()
+            }
+            Spacer()
+        }
+    }
+
+    private func emoji(_ value: Int) -> String {
+        guard let scalar = UnicodeScalar(value) else { return "?" }
+        return String(Character(scalar))
     }
 }
 
