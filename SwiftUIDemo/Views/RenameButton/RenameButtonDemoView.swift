9076bc019df6d598aaf4cade7e3efaee229143c8
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-06 10:39:01 -0800

    Adding RenameButton demo view

--- SwiftUIDemo/Views/RenameButton/RenameButtonDemoView.swift
+++ SwiftUIDemo/Views/RenameButton/RenameButtonDemoView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct RenameButtonDemoView: View {
+    @State private var text = ""
+    @FocusState private var isFocused: Bool
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(RenameButtonDemoView)")
+        /*
+         This code is adapted from Apple's documentation
+         https://developer.apple.com/documentation/swiftui/renamebutton
+         
+         Note: RenameButton is currently only available in iOS 16.0+
+         */
+        if #available(iOS 16, *) {
+            VStack {
+                TextField(text: $text) {
+                    Text("Prompt")
+                }
+                .focused($isFocused)
+                
+                if text.count > 0 {
+                    RenameButton()
+                        .renameAction { isFocused = true }
+                } else {
+                    // No rename action?  The button will be disabled.
+                    RenameButton()
+                }
+            }
+            
+        } else {
+            Text("Placeholder View")
+            Text("(RenameButtonDemoView)")
+        }
     }
 }
 
