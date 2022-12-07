5d0b91f2a0f73ef663e11ccb9edf8213d21e760f
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-07 15:58:17 -0800

    Adding TransformedShape demo view

--- SwiftUIDemo/Views/TransformedShape/TransformedShapeDemoView.swift
+++ SwiftUIDemo/Views/TransformedShape/TransformedShapeDemoView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct TransformedShapeDemoView: View {
+    private let transform: CGAffineTransform
+    
+    init() {
+        if #available(iOS 16, *) {
+            /*
+             This does not currently work:
+             "Ambiguous use of 'init(scale:horizontalShear:rotation:translation:)'"
+             */
+//            let transformComponents = CGAffineTransformComponents(
+//                scale: CGSize(width: 50, height: 50),
+//                horizontalShear: 0.0,
+//                rotation: 0.0,
+//                translation: CGVector(dx: 0, dy: 0))
+//
+//            transform = CGAffineTransform(transformComponents)
+        }
+        transform = CGAffineTransform(0.5, 0.25, 0.25, 0.5, 50, 20)
+    }
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(TransformedShapeDemoView)")
+        TransformedShape(shape: Rectangle(), transform: CGAffineTransform(scaleX: 0.5, y: 0.5))
+            .padding(32)
+        
+        if #available(iOS 16, *) {
+            TransformedShape(shape: Rectangle(),transform: transform)
+        }
     }
 }
 
