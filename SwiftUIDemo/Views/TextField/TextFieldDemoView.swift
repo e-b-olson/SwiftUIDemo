da5c82d4f1bae7714546c366118dbd9ddaf8a8dd
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-07 14:24:03 -0800

    Adding TextField demo view

--- SwiftUIDemo/Views/TextField/TextFieldDemoView.swift
+++ SwiftUIDemo/Views/TextField/TextFieldDemoView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct TextFieldDemoView: View {
+    @State private var text: String = ""
+    @State private var isValid: Bool = true
+    @FocusState private var textFieldFocusState: Bool
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(TextFieldDemoView)")
+        TextField(
+                "Placeholder text...",
+                text: $text
+            )
+            .focused($textFieldFocusState)
+            .onSubmit {
+                isValid = customValidation(text)
+            }
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
+            .border(.secondary)
+            .padding(32)
+
+            Text("You wrote: " + text)
+                .foregroundColor(isValid ? .blue : .red)
+                .padding(.horizontal, 32)
+    }
+    
+    func customValidation(_ input: String) -> Bool {
+        return input.count > 3
     }
 }
 
