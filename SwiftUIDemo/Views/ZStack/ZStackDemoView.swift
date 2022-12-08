8f2fcc73f4ae52dce605ffc7ae43bd88b8a282f1
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-08 12:12:14 -0800

    Adding ZStack demo view

--- SwiftUIDemo/Views/ZStack/ZStackDemoView.swift
+++ SwiftUIDemo/Views/ZStack/ZStackDemoView.swift
@@ -8,10 +8,26 @@
 import SwiftUI
 
 struct ZStackDemoView: View {
+    @State private var yOffset: Double = 0
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(ZStackDemoView)")
+        ZStack {
+            Circle()
+                .foregroundColor(Color.cyan)
+                .scaleEffect(0.3)
+            
+            Text("ZStacks")
+                .offset(x: 0, y: yOffset * -1)
+            Text("allow you to")
+                .offset(x: 0, y: yOffset * 0)
+            Text("layer views in the z-plane")
+                .offset(x: 0, y: yOffset * 1)
+            Text("(like a deck of cards)")
+                .offset(x: 0, y: yOffset * 2)
+        }
+        
+        Slider(value: $yOffset, in: 0...32, label: { Text("Offset")} )
+            .padding(.horizontal, 32)
     }
 }
 
