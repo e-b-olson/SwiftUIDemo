f809d16e297d752e90b883104823cc8a59bbe83c
e.b.olson <e.b.olson.mobile@gmail.com>
2022-12-08 17:37:18 -0800

    Adding DefaultDateProgressLabel demo view (kinda)

--- SwiftUIDemo/Views/DefaultDateProgressLabel/DefaultDateProgressLabelDemoView.swift
+++ SwiftUIDemo/Views/DefaultDateProgressLabel/DefaultDateProgressLabelDemoView.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 struct DefaultDateProgressLabelDemoView: View {
+    @State private var timerDateRange = Date()...Date().addingTimeInterval(30)
     
     var body: some View {
-        Text("Placeholder View")
-        Text("(DefaultDateProgressLabelDemoView)")
+        /*
+         As far as I can tell, DefaultDateProgressLabel is an internal
+         type (used by ProgressView).  I don't think you would ever
+         explicitly create a DefaultDateProgressLabel view yourself.
+         
+         Please see the Apple Documentation for more info:
+         https://developer.apple.com/documentation/swiftui/defaultdateprogresslabel
+         */
+        if #available(iOS 16, *) {
+            ProgressView(timerInterval: timerDateRange) {
+                Text("Timer")
+            }
+            .padding(32)
+            
+            Button("Reset", action: {
+                timerDateRange = Date()...Date().addingTimeInterval(30)
+            })
+        } else {
+            Text("Placeholder View")
+            Text("(DefaultDateProgressLabelDemoView)")
+        }
     }
 }
 
